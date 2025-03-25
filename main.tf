b39dc008a0be3f5f630966d8b8b1659e2a0fe603
DJENJNY <ethan.jon32@gmail.com>
2025-03-25 12:08:14 -0500

    added networking components

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket = "3tier-terraform32"
+    key    = "terraform"
+    region = "us-east-1"
+  }
+
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,3 +17,54 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "3tiervpc"
+  }
+}
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "Pubsub1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet 1"
+  }
+}
+
+resource "aws_subnet" "Pubsub2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "subnet 2"
+  }
+}
+
+resource "aws_route_table" "pubrt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+
