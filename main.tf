277e8572924ede64e2c93b63ccae5a78e5ece8ff
DJENJNY <ethan.jon32@gmail.com>
2025-04-01 20:16:06 -0400

    added EC2 IAM role and attached policies

--- main.tf
+++ main.tf
@@ -17,6 +17,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_instance_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_read_only_policy" {
+  
+  role      = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess" # Example policy
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonSSM_Managed_Instance_Core" {
+  
+  role      = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore" # Example policy
+}
+
 # Create a VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
